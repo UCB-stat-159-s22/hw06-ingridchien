f18625a07974d5917f9239078950f41e551a7021
Ingrid Chien <ingrid070401@berkeley.edu>
2022-04-20 04:36:48 +0000

    add ligotools version

---
+++ ligotools/tests/test_readligo.py
@@ -0,0 +1,11 @@
+'''
+import numpy as np
+
+def test_np():
+    assert np.abs(-5) == 5
+'''
+    
+from ligotools import *
+
+
+    
--- ligotools/tests/tests_readligo.py
+++
@@ -1,4 +0,0 @@
-from ligotools import readligo
-
-def test_loaddata():
-    
